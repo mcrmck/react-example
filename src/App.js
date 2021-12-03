ee181d514e7c469e319a8bf7882e1a15010bf439
ethanmthomas17 <ethanmthomas17@gmail.com>
2021-12-03 17:17:25 -0500

    Added dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import PageContent from "./components/PageContent";
+import Dashboard from "./components/Dashboard";
+// import Sidebar from "./components/Sidebar";
+import React from "react";
 
 function App() {
+  // const drawerWidth = 240;
+  // const [open, setOpen] = React.useState(true);
+  // const toggleDrawer = () => {
+  //   setOpen(!open);
+  // };
+
   return (
     <div>
+      <Dashboard />
       <PageContent width="80%" />
     </div>
   );
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,12 +1,40 @@
 import OzoneOverTime from "./plots/OzoneOverTime";
 import OzoneOverSolar from "./plots/OzoneOverSolar";
+import React from "react";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 function PageContent({ width }) {
+
   return (
-    <div>
-      <OzoneOverTime width={width} />
-      <OzoneOverSolar width={width} />
-    </div>
+    <Box
+    component="main"
+    sx={{
+      backgroundColor: (theme) =>
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[900],
+      flexGrow: 1,
+      height: "100vh",
+      overflow: "auto",
+    }}
+  >
+    <Toolbar />
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        {/* Chart */}
+        <Grid item xs={12} md={8} lg={12}>
+          <OzoneOverSolar />
+        </Grid>
+        <Grid item xs={12} md={8} lg={12}>
+          <OzoneOverTime />
+        </Grid>
+      </Grid>
+      {/* <Copyright sx={{ pt: 4 }} /> */}
+    </Container>
+  </Box>
   );
 }
 
